40635416bc20a399c43f898afb473e2f30cc3f83
hcyspeech <aaxxdd74@gmail.com>
2022-02-02 01:32:02 +0900

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Meme project
           </a>
         </nav>
         <div className="container-fluid mt-5">
